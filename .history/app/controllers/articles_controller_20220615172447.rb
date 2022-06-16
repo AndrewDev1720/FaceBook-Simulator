098055ee956a389a085e04eb07e0aaedfbcca1ea
quan <quan200202@gmail.com>
2022-06-16 13:57:00 +0700

    Full CRUD

---
+++ .history/app/controllers/articles_controller_20220615172447.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  def show()
+    @article = Article.find(params[:id])
+  end
+
+  def index()
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new()
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save 
+      flash[:notice] = "Article was created Successfully"
+      redirect_to article_path(@article.id)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if (@article.update(params.require(:article).permit(:title, :description)))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+      # redirect_to edit_article_path
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    redirect_to 'https://getbootstrap.com/docs/4.5/content/tables/'
+  end
+
+end
---
+++ .history/app/controllers/articles_controller_20220616103254.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  def show()
+    getArticle()
+  end
+
+  def index()
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new()
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save 
+      flash[:notice] = "Article was created Successfully"
+      redirect_to article_path(@article.id)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    getArticle()
+  end
+
+  def update
+    getArticle()
+    if (@article.update(params.require(:article).permit(:title, :description)))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+      # redirect_to edit_article_path
+    end
+  end
+
+  def destroy
+    getArticle()
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private def getArticle()
+    @article = Article.find(params[:id])
+  end
+  
+
+end
